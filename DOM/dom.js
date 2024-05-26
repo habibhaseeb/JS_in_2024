00add28aa476b15a2dfebd35902aac1f60c92d02
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-05-26 14:16:33 +0000

    JS in 2024

---
+++ DOM/dom.js
@@ -0,0 +1,15 @@
+// ================================ Dom styling ============================================
+// let container = document.getElementById("container");
+// let element = document.createElement('div');
+// let NewContainer = container.appendChild(element);
+// NewContainer.textContent = "hello";
+// NewContainer.style.margin = "10px"
+// NewContainer.style.display = 'flex',NewContainer.style.justifyContent = 'center', NewContainer
+// .style.border = '2px solid black';
+// let check = element.parentElement;
+// console.log(`your element created: ${check}` + console.dir(check));
+
+// ------------------------------------------------------------------------------------//
+let string = "hello \n  this \'is string\'";
+let formatedString =  string;
+console.log(formatedString);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# JAVASCRIPT in 2024
+ This code contains diffrent practices in webdevelopment, mainly JavaScript.
+ And its code is shared on my github profile. Review my code and done best practices.
