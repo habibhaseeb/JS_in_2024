de6fc89b65b29fd9e6f74daba560ea31d79eef17
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-06-16 20:11:03 +0500

    Update and rename js1.js to naturalNumbers.js

---
+++ naturalNumbers.js
@@ -0,0 +1,14 @@
+let userInput = parseInt  
+  (prompt("Enter the +ve natural number: "));  
+let sumOfIndex = 0;   
+
+let updatedIndex = 1;  
+while(updatedIndex <= userInput){ 
+  
+     sumOfIndex += updatedIndex;     // warning you are alert of this error like 1 or i,   
+  //  but always put i or (updatedIndex)    updatedIndex++;}console.log("The sum of Natural numbers are :" , sumOfIndex);
+  // Note: This code is not executed in terminal because the user can't give input from terminal 
+  // its always be executed in inspect element of the console of browser.. great..
+};
+
+console.log("The sum of natural numbers are :" , sumOfIndex);
