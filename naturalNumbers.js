5e2fbf1c1aa0380e40fd351a02860606c7bea084
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-06-16 08:13:09 -0700

    Update naturalNumbers.js

--- naturalNumbers.js
+++ naturalNumbers.js
@@ -6,7 +6,8 @@ let updatedIndex = 1;
 while(updatedIndex <= userInput){ 
   
      sumOfIndex += updatedIndex;     // warning you are alert of this error like 1 or i,   
-  //  but always put i or (updatedIndex)    updatedIndex++;}console.log("The sum of Natural numbers are :" , sumOfIndex);
+  //  but always put i or (updatedIndex)    updatedIndex++;}
+console.log("The sum of Natural numbers are :" , sumOfIndex);
   // Note: This code is not executed in terminal because the user can't give input from terminal 
   // its always be executed in inspect element of the console of browser.. great..
 };
